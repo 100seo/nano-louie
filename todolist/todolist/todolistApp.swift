ac92bd3533f2e939eb4e2b6c9478165c31153c63
100seo <deepgreen7362@gmail.com>
2022-04-28 02:08:35 +0900

    new swiftui file
    스위프트 파일을 새로 만들고 스플래시뷰를 추가해봄

---
+++ todolist/todolist/todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  todolistApp.swift
+//  todolist
+//
+//  Created by baek seohyeon on 2022/04/28.
+//
+
+import SwiftUI
+
+@main
+struct todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
